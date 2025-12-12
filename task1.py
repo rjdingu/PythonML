17e4be20ca9b6a75fba1714441f7a71900dc29b2
rjdingu <rj.dingu@gmail.com>
2025-12-12 19:49:55 +0530

    These invovles the basic practices of Python

---
+++ task1.py
@@ -0,0 +1,41 @@
+# Sum of all even number in a list
+
+def sumEven(l1):
+    sum = 0
+    for i in l1:
+        if i%2 == 0:
+            sum = sum + i
+
+    return sum
+
+l = list(map(int,input("Enter a list : ").split()))
+result = sumEven(l)
+print(result)
+
+# Count the number of Vowels
+
+def countVowels(words):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for i in words:
+        if i in vowels:
+            count+=1
+    return count
+
+w = input("Enter a words :")
+resultCount = countVowels(w)
+print(resultCount)
+
+
+# Print all unique Numbers of a list
+
+def uniqueNums(nums):
+    for i in range(0,len(nums)-1):
+        if i in nums and nums.count(i) >=2:
+            nums.remove(i)    
+    return nums
+
+l = [1,1,1,2,2,3,3,4]
+uniqueList = uniqueNums(l)
+print(uniqueList)                
+
---
+++ task3.py
@@ -0,0 +1,27 @@
+# List compreshsion
+# Divisible by 4 from 0 to 5 :
+def diviFour():
+    l = [i for i in range(0,51) if i%4 == 0]
+    print(l)
+
+diviFour()
+
+# Upper case all the string in a list :
+def upper(l1):
+    l2 = [i.upper() for i in l1 ]
+    print(l2)
+
+l = ['hia','raja','how']
+upper(l)
+
+# Print only the string contain 'a' in the list :
+def onlya(l):
+    l2 = [i for i in l if 'a' in i]
+    print(l2)
+
+onlya(l)
+
+
+# Print the 5*5 matrix
+matrix_5x5 = [[0 for j in range(5)] for i in range(5)]
+print(matrix_5x5)
